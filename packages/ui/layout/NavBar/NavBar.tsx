b9afc4c191b85efc49f73d9e1617fa0f49eeab1f
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2024-10-20 16:56:54 +0900

    chore: set icons

--- packages/ui/layout/NavBar/NavBar.tsx
+++ packages/ui/layout/NavBar/NavBar.tsx
@@ -1,13 +1,40 @@
 "use client";
 
-import Image from "next/image";
+import AiTalkIcon from "@/packages/icons/AiTalk.svg";
+import AiTalkIconActive from "@/packages/icons/AiTalkActive.svg";
+import HomeIcon from "@/packages/icons/Home.svg";
+import HomeIconActive from "@/packages/icons/HomeActive.svg";
+import MagazineIcon from "@/packages/icons/Magazine.svg";
+import MagazineIconActive from "@/packages/icons/MagazineActive.svg";
+import MyPageIcon from "@/packages/icons/Mypage.svg";
+import MyPageIconActive from "@/packages/icons/MypageActive.svg";
 import { usePathname, useRouter } from "next/navigation";
 
 const navItems = [
-  { path: "/home", label: "홈", img: "/assets/images/navbtn" },
-  { path: "/AiTalk", label: "AI 토크", img: "/assets/images/navbtn" },
-  { path: "/article", label: "매거진", img: "/assets/images/navbtn" },
-  { path: "/myPage", label: "마이페이지", img: "/assets/images/navbtn" },
+  {
+    path: "/home",
+    label: "홈",
+    SvgIcon: HomeIcon,
+    SvgIconActive: HomeIconActive,
+  },
+  {
+    path: "/AiTalk",
+    label: "AI 토크",
+    SvgIcon: AiTalkIcon,
+    SvgIconActive: AiTalkIconActive,
+  },
+  {
+    path: "/article",
+    label: "매거진",
+    SvgIcon: MagazineIcon,
+    SvgIconActive: MagazineIconActive,
+  },
+  {
+    path: "/myPage",
+    label: "마이페이지",
+    SvgIcon: MyPageIcon,
+    SvgIconActive: MyPageIconActive,
+  },
 ];
 
 export const NavBar = () => {
@@ -17,23 +44,14 @@ export const NavBar = () => {
   return (
     <nav className="fixed bottom-0 w-full h-[72px] bg-[#fdfdff] shadow-md border-t border-t-gray-200 z-50">
       <ul className="flex justify-around items-center h-full">
-        {navItems.map(({ path, label, img }) => (
+        {navItems.map(({ path, label, SvgIcon, SvgIconActive }) => (
           <li
             key={path}
             onClick={() => router.push(path)}
             className="cursor-pointer"
           >
             <div className="flex flex-col items-center">
-              <Image
-                src={
-                  currentPath.startsWith(path)
-                    ? `${img}_active.png`
-                    : `${img}_default.png`
-                }
-                alt={label}
-                width={28}
-                height={28}
-              />
+              {currentPath.startsWith(path) ? <SvgIconActive /> : <SvgIcon />}
               <span className="text-[#8b8d98] mt-2 text-[10px] font-semibold">
                 {label}
               </span>
